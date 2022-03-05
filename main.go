0f6c4b7942e7af2e5edc70316e4bf7845fca069e
Egor Dubin <delisa.sama@gmail.com>
2022-03-05 17:18:35 +0300

    Add reverse proxy

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
+	"strconv"
 
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
@@ -13,14 +16,49 @@ func HealthCheck(ctx *fasthttp.RequestCtx) {
 	ctx.WriteString("it's work")
 }
 
+func MakeProxyHandler(uri *fasthttp.URI) fasthttp.RequestHandler {
+	client := &fasthttp.Client{}
+	return func(ctx *fasthttp.RequestCtx) {
+		req := &ctx.Request
+		req.SetURI(uri)
+		err := client.Do(&ctx.Request, &ctx.Response)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
 func main() {
 	r := router.New()
 	r.GET("/health-check", HealthCheck)
 
-	_, err := config.LoadGlobal()
+	global, err := config.LoadGlobal()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Fatal(fasthttp.ListenAndServe(":8080", r.Handler))
+	services, err := config.LoadServices()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", services)
+	for _, service := range services {
+		for _, endpoint := range service.Endpoints {
+			uri := &fasthttp.URI{}
+			err := uri.Parse(nil, []byte(fmt.Sprintf("%s/%s", service.Host, endpoint.To)))
+			if err != nil {
+				log.Fatal(err)
+			}
+			r.Handle(endpoint.Method, endpoint.From, MakeProxyHandler(uri))
+		}
+	}
+
+	err = fasthttp.ListenAndServe(
+		net.JoinHostPort(
+			global.Entrypoint.Host,
+			strconv.Itoa(global.Entrypoint.Port),
+		),
+		r.Handler,
+	)
+	log.Fatal(err)
 }
